3d32d57c002b388ab907dc47f48391b31242c1da
Siddharth Verma <siddharthverma3904@gmail.com>
2023-09-11 17:35:48 +0530

    mouse movement using eyes but difficulty

--- viual_mouse.py
+++ viual_mouse.py
@@ -1,21 +1,47 @@
 import cv2 as cv
+import mediapipe as mp
 import pyautogui as pg
 
+mesh = mp.solutions.face_mesh
+W, H = pg.size()
 live = cv.VideoCapture(0)
-eye_harr_cascade = cv.CascadeClassifier('eyetrack.xml')
 
-while True :
-    isTrue, frame = live.read()
-    invert = cv.flip(frame, 1)
-    gray = cv.cvtColor(invert, cv.COLOR_BGR2GRAY)
-    eye_track = eye_harr_cascade.detectMultiScale(gray)
-    for x, y, w, h in eye_track :
-        cv.rectangle(invert, (x, y), (x+w, y+h), (0, 255, 0), 1)
-    if isTrue :
-        cv.imshow("Live Camera", invert)
-        if cv.waitKey(20) & 0xFF == ord('d') :
+with mesh.FaceMesh(refine_landmarks=True) as face_mesh :
+  while live.isOpened():
+    success, frame = live.read()
+    if success:
+        # cropped = frame[frame.shape[1]//4 : 3*frame.shape[1]//4, frame.shape[0]//4 : 3*frame.shape[0]//4]
+        # frame = cv.flip(cropped, 1)
+        frame = cv.flip(frame, 1)
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        output = face_mesh.process(frame)
+        points = output.multi_face_landmarks
+        fH, fW, fZ = frame.shape
+        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        if points :
+            marks = points[0].landmark
+            for i, landmark in enumerate(marks[474:478]) :
+                x = int(landmark.x * fW)
+                y = int(landmark.y * fH)
+                cv.circle(frame, (x, y), 3, (0, 255, 0), 1)
+                if i==1 :
+                    mouseX = W * landmark.x
+                    mouseY = H * landmark.y
+                    print(mouseX, mouseY)
+                    pg.moveTo(mouseX, mouseY)
+                lefteye = [marks[145], marks[159]]
+                for landmark in lefteye :
+                    x = int(landmark.x * fW)
+                    y = int(landmark.y * fH)
+                    cv.circle(frame, (x, y), 3, (255, 0, 0), 1)
+                if (lefteye[0].y - lefteye[1].y) <0.004 :
+                    print("left eye blinked")
+                    pg.click()
+                    pg.sleep(1)
+            cv.imshow('Camera', frame)
+        if cv.waitKey(20) & 0xFF == ord('d'):
             break
     else :
-        break
+        continue
 live.release()
 cv.destroyAllWindows()
